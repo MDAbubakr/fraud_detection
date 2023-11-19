d499e4bd59469051beddccfa2c00427b0a579143
MDAbubakr <er.abubakr@gmail.com>
2023-11-19 14:47:53 +0530

    revert "added a new function called multiplication"
    This reverts commit 84a7ca7ec86e271f1661f5d796f13abda68b7dbd.

--- data_processing.py
+++ data_processing.py
@@ -1,12 +1,2 @@
 
 print("adding all the data processing code related to fraud detection")
-
-
-a = 7
-b= 9
-
-def multiplication(a,b):
-    c = a * b
-    return c
-
-print('added new function called multiplication')
